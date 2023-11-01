e8f4b9032b1d615b309d7cd1b0f357e5f03179f3
MarioMRdev <mrmartinezr47@mail.com>
2023-11-01 00:01:31 -0500

    Proyecto de ionic

---
+++ Proyecto/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Share } from '@capacitor/share';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Customers', url: '/customers', icon: 'people' },
+    { title: 'Cities', url: '/cities', icon: 'location' },
+    { title: 'Login', url: '/login', icon: 'log-in' }
+  ];
+  constructor() {}
+
+  shareApp(){
+    Share.share({
+      title: 'Nueva App',
+      text: 'Descaraga la nueva app',
+      url: 'http://ionicframework.com/',
+    });
+  }
+}
